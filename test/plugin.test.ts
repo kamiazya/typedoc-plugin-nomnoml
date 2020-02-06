4fce7357586b0500ede25da28dc781b20ac80959
kamiazya <yuki@kamiazya.tech>
2020-02-06 22:05:38 +0900

    fix: fix test for plugin

--- test/plugin.test.ts
+++ test/plugin.test.ts
@@ -20,34 +20,9 @@ describe('NomnomlPlugin', () => {
     const input = '</body>';
     const result = plugin.convertPageContents(input);
     expect(result).toMatch('</body>');
-    expect(result).toMatch(
-      /<script src=\"https:\/\/cdnjs.cloudflare.com\/ajax\/libs\/dagre\/[\d|.]+\/dagre.min.js\"><\/script>/,
-    );
-    expect(result).toMatch(
-      /<script src=\"https:\/\/cdnjs.cloudflare.com\/ajax\/libs\/nomnoml\/[\d|.]+\/nomnoml.min.js\"><\/script>/,
-    );
-  });
-
-  // tslint:disable-next-line: quotemark
-  it("can be change nomnoml.js version when set 'nomnomlVersion' option", () => {
-    plugin.application.options.setValue('nomnomlVersion', '0.0.0');
-
-    const input = '</body>';
-    const result = plugin.convertPageContents(input);
-    expect(result).toMatch('</body>');
-    expect(result).toMatch(
-      /<script src=\"https:\/\/cdnjs.cloudflare.com\/ajax\/libs\/nomnoml\/0\.0\.0\/nomnoml.min.js\"><\/script>/,
-    );
-  });
-
-  // tslint:disable-next-line: quotemark
-  it("load nomnoml@0.6.1 when not to set 'nomnomlVersion' option", () => {
-    const input = '</body>';
-    const result = plugin.convertPageContents(input);
-    expect(result).toMatch('</body>');
-    expect(result).toMatch(
-      /<script src=\"https:\/\/cdnjs.cloudflare.com\/ajax\/libs\/nomnoml\/0\.6\.1\/nomnoml.min.js\"><\/script>/,
-    );
+    expect(result).toContain('<script src="https://cdn.jsdelivr.net/npm/dagre/dist/dagre.min.js"></script>');
+    expect(result).toContain('<script src="https://cdn.jsdelivr.net/npm/nomnoml/dist/nomnoml.min.js"></script>');
+    expect(result).toMatchSnapshot();
   });
 
   it('convert PageContents returns same value if body closing tag not exixt', () => {
